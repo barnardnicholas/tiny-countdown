ce290d48de9382cccd8d745854b838cd2c97b73d
Nick <nick@dawncreative.co.uk>
2021-02-03 18:34:20 +0000

    countdown displays values from current time

--- countdown.js
+++ countdown.js
@@ -4,6 +4,35 @@ const updateViewport = () => {
   document.documentElement.style.setProperty("--vh", `${vh}px`);
 };
 
+const cdYears = document.getElementById("cd-num-years");
+const cdMonths = document.getElementById("cd-num-months");
+const cdWeeks = document.getElementById("cd-num-weeks");
+const cdDays = document.getElementById("cd-num-days");
+const cdHours = document.getElementById("cd-num-hours");
+const cdMins = document.getElementById("cd-num-mins");
+const cdSecs = document.getElementById("cd-num-secs");
+// const timeElements = [cdYears, cdMonths, cdWeeks, cdDays, cdHours, cdMinutes, cdSecs];
+
+const updateCountdown = (dateMS) => {
+  const dateToShow = new Date(dateMS);
+  const years = dateToShow.getYear() || "00";
+  const months = dateToShow.getMonth() + 1 || "00";
+  const weeks = "00";
+  const days = dateToShow.getDay() || "00";
+  const hours = dateToShow.getHours() || "00";
+  const mins = dateToShow.getMinutes() || "00";
+  const secs = dateToShow.getSeconds() || "00";
+
+  console.log(years);
+  cdYears.innerText = years;
+  cdMonths.innerText = months;
+  cdWeeks.innerText = weeks;
+  cdDays.innerText = days;
+  cdHours.innerText = hours;
+  cdMins.innerText = mins;
+  cdSecs.innerText = secs;
+};
+
 const now = new Date();
 // MS UTC
 console.log(now.getTime());
@@ -14,6 +43,7 @@ console.log(now.toLocaleTimeString());
 console.log(now.getTimezoneOffset());
 
 // On window load
-window.onLoad = (event) => {
-  updateViewport();
-};
+window.onLoad = (event) => {};
+
+updateViewport();
+updateCountdown(now.getTime());
