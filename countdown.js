1c7b426794a4badba2e7637d5c1494a1cf44e1c6
Nick <nick@dawncreative.co.uk>
2021-02-04 09:17:16 +0000

    working countdown based on query

--- countdown.js
+++ countdown.js
@@ -9,6 +9,34 @@ const msVals = {
   sec: 1000,
 };
 
+// Queries
+const ezQuery = () => {
+  // Get URL query
+  const queryString = location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  // Utils
+  // Convert to integer if necessary
+  const detectInteger = (item) => {
+    if (Number.isInteger(parseInt(item))) return parseInt(item);
+    else return item;
+  };
+
+  // Assemble Query Object
+  const queryObj = {};
+  for (let pair of urlParams.entries()) {
+    const key = pair[0];
+    let value = pair[1];
+    if (value.split(",").length > 1) {
+      value = value.split(",").map(detectInteger);
+    } else {
+      value = detectInteger(value);
+    }
+    queryObj[key] = value;
+  }
+  return queryObj;
+};
+
 // Define VH Unit correctly (mobile bug)
 const updateViewport = () => {
   let vh = window.innerHeight * 0.01;
@@ -32,14 +60,6 @@ const timeFromMS = (ms) => {
   }
 };
 
-// const cdYears = document.getElementById("cd-num-years");
-// const cdMonths = document.getElementById("cd-num-months");
-const cdWeeks = document.getElementById("cd-num-weeks");
-const cdDays = document.getElementById("cd-num-days");
-const cdHours = document.getElementById("cd-num-hours");
-const cdMins = document.getElementById("cd-num-mins");
-const cdSecs = document.getElementById("cd-num-secs");
-
 // const cdSectYears = document.getElementById("cd-sect-years");
 // const cdSectMonths = document.getElementById("cd-sect-months");
 const cdSectWeeks = document.getElementById("cd-sect-weeks");
@@ -48,14 +68,26 @@ const cdSectHours = document.getElementById("cd-sect-hours");
 const cdSectMins = document.getElementById("cd-sect-mins");
 const cdSectSecs = document.getElementById("cd-sect-secs");
 
-const testTime = 1612377349733 + 86400000;
-console.log(`Test time (MS): ${testTime}`);
-const transTestTime = timeFromMS(testTime);
-console.log(`Translated:`);
-console.dir(transTestTime);
+// const cdYears = document.getElementById("cd-num-years");
+// const cdMonths = document.getElementById("cd-num-months");
+const cdWeeks = document.getElementById("cd-num-weeks");
+const cdDays = document.getElementById("cd-num-days");
+const cdHours = document.getElementById("cd-num-hours");
+const cdMins = document.getElementById("cd-num-mins");
+const cdSecs = document.getElementById("cd-num-secs");
 
-const updateCountdown = (ms) => {
-  const time = timeFromMS(ms);
+// const cdLabYears = document.getElementById("cd-lab-years");
+// const cdSectMonths = document.getElementById("cd-lab-months");
+const cdLabWeeks = document.getElementById("cd-lab-weeks");
+const cdLabDays = document.getElementById("cd-lab-days");
+const cdLabHours = document.getElementById("cd-lab-hours");
+const cdLabMins = document.getElementById("cd-lab-mins");
+const cdLabSecs = document.getElementById("cd-lab-secs");
+
+const updateCountdown = () => {
+  const now = new Date();
+  let timeOffset = ezQuery().t - now.getTime();
+  const time = timeFromMS(timeOffset);
   // const years = dateToShow.getYear() || "00";
   // const months = dateToShow.getMonth() + 1 || "00";
   const weeks = time.weeks || 0;
@@ -67,6 +99,8 @@ const updateCountdown = (ms) => {
   if (weeks || ms > msVals.week) {
     cdSectWeeks.style.display = "initial";
     cdWeeks.innerText = weeks;
+    if (weeks > 1) cdLabWeeks.innerText = "weeks";
+    else cdLabWeeks.innerText = "week";
   } else {
     cdSectWeeks.style.display = "none";
   }
@@ -74,6 +108,8 @@ const updateCountdown = (ms) => {
   if (days || ms > msVals.day) {
     cdSectDays.style.display = "initial";
     cdDays.innerText = days;
+    if (days > 1) cdLabDays.innerText = "days";
+    else cdLabDays.innerText = "day";
   } else {
     cdSectDays.style.display = "none";
   }
@@ -81,6 +117,8 @@ const updateCountdown = (ms) => {
   if (hours || ms > msVals.hour) {
     cdSectHours.style.display = "initial";
     cdHours.innerText = hours;
+    if (hours > 1) cdLabHours.innerText = "hours";
+    else cdLabHours.innerText = "hour";
   } else {
     cdSectHours.style.display = "none";
   }
@@ -88,6 +126,8 @@ const updateCountdown = (ms) => {
   if (mins || ms > msVals.min) {
     cdSectMins.style.display = "initial";
     cdMins.innerText = mins;
+    if (mins > 1) cdLabMins.innerText = "minutes";
+    else cdLabMins.innerText = "minute";
   } else {
     cdSectMins.style.display = "none";
   }
@@ -95,6 +135,8 @@ const updateCountdown = (ms) => {
   if (secs || ms > msVals.sec) {
     cdSectSecs.style.display = "initial";
     cdSecs.innerText = secs;
+    if (secs > 1) cdLabSecs.innerText = "seconds";
+    else cdLabSecs.innerText = "second";
   } else {
     cdSectSecs.style.display = "none";
   }
@@ -117,5 +159,17 @@ console.log(now.getTimezoneOffset());
 // On window load
 window.onLoad = (event) => {};
 
+const testTime = 1612377349733 + 86400000;
+console.log(`Test time (MS): ${testTime}`);
+const transTestTime = timeFromMS(testTime);
+console.log(`Translated:`);
+console.dir(transTestTime);
+
+let timeOffset = ezQuery().t - now.getTime();
+
 updateViewport();
-updateCountdown(msVals.day * 8);
+
+updateCountdown();
+setInterval(() => {
+  updateCountdown();
+}, 1000);
